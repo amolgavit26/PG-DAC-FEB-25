28042c1edb7d63d05d85288a8a802adf2c303abc
Amol Gavit <amolgavit158121@gmail.com>
2025-02-20 22:04:48 +0530

    Add files via upload

---
+++ Logic_Building/Day_1/Assignment1.java
@@ -0,0 +1,285 @@
+import java.util.Scanner;
+
+class Assignment1{
+
+	public static void main(String args[])
+	{
+	
+
+	
+/*
+	System.out.println("Hello\nAmol Gavit");	//Q1 "\n is for Line Brake	
+	
+
+	int a = 74;
+	int b = 36;					//Q2
+	System.out.println(a+b);
+	
+
+
+	int c = 50;
+	int d = 3;					//Q3
+	System.out.println(c/d);
+
+
+
+
+	int e = 5;
+	int f = 8;
+	int g = 6;
+	int h = 55;
+	int i = 9;					//Q4
+	int j = 20;
+	int k = 3;
+	int l = 15;
+	int m = 2;
+	System.out.println(-e+f*g);
+	System.out.println((h+i)%i);
+	System.out.println(j-k*e/f);
+	System.out.println(e+l/k*m-f%k);
+
+
+
+
+
+	Scanner x = new Scanner(System.in);
+	
+	System.out.println("Enter First Number:");	//Q5
+	int first = x.nextInt();
+	System.out.println("Enter Second Number:");
+	int second = x.nextInt();
+
+	System.out.println(first*second);
+
+
+	Scanner x = new Scanner(System.in);
+	
+	System.out.println("Enter First Number:");	//Q6
+	int first = x.nextInt();
+	System.out.println("Enter Second Number:");
+	int second = x.nextInt();
+
+	System.out.println(first+second);
+	System.out.println(first-second);
+	System.out.println(first*second);
+	System.out.println(first/second);
+	System.out.println(first%second);
+	
+
+	
+	Scanner x = new Scanner(System.in);
+	
+	System.out.println("Enter the Number:");	//Q7
+	int first = x.nextInt();	
+	System.out.println(first);
+	for (int z = 0; z < 10; z++)
+	{System.out.println(first + " X " + (z + 1) + " = " + (first * (z + 1)));
+	}
+
+	
+
+	Scanner x = new Scanner(System.in);
+	
+	System.out.println("Enter First Number:");	//Q8
+	int first = x.nextInt();
+	System.out.println("Enter Second Number:");
+	int second = x.nextInt();
+
+	first = first + second;
+	second = first - second;
+	first = first - second;
+	
+	System.out.println("first = " + first + " second = " + second);
+
+
+	
+	Scanner x = new Scanner(System.in);
+
+	System.out.println("Enter The Radius of Circle");
+	int radius = x.nextInt();						//Q9
+	System.out.println("Area of Circle is : "+(3.14*(radius*radius)));
+
+
+
+
+
+	Scanner x = new Scanner(System.in);	
+
+	System.out.println("Enter the Number");
+	int Number = x.nextInt();
+	
+	if (Number % 2 == 0){						//Q10
+	System.out.println("The number "+Number+" is Even");
+	}
+	else{
+	System.out.println("The number "+Number+" is Odd");
+	}
+
+
+
+	Scanner x = new Scanner(System.in);
+
+	System.out.println("Enter First Number");
+	int First = x.nextInt();
+	System.out.println("Enetr the Second Number");
+	int Second = x.nextInt();
+	System.out.println("Enetr Third Number");
+	int Third = x.nextInt();
+
+	if (First >= Second && First >= Third){				//Q11
+	System.out.println("The number "+First+" is Largest");
+	}
+	
+	else if (Second >= First && Second >= Third){
+	System.out.println("The number "+Second+" is Largest");
+	}
+
+	else{
+	System.out.println("The number "+Third+" is Largest");
+	}
+
+
+
+	Scanner x = new Scanner(System.in);
+
+	System.out.print("Enter thr number you want to reverse");
+	int Number = x.nextInt();
+
+	int Reverse = 0;
+	int Remainder;						//Q12
+
+	while (Number>0){
+		Remainder = Number%10;
+		Reverse = (Reverse*10) + Remainder;
+		Number = Number/10;
+		
+	}
+
+	System.out.println("Reversed Number is "+ Reverse);
+
+
+
+
+	Scanner x = new Scanner(System.in);
+
+	System.out.println("Enter First Number");
+	int First = x.nextInt();
+	System.out.println("Enter Second Number");		//Q13
+	int Second = x.nextInt();
+	System.out.println("Enter Third Number");
+	int Third = x.nextInt();
+
+	System.out.println("The average is : "+(First+Second+Third)/3);
+
+
+
+
+
+
+	Scanner x = new Scanner(System.in);
+	
+	System.out.println("Eneter the number till you want the fibonacci series");
+	int Number = x.nextInt();
+
+	int num1 = 0, num2 = 1;
+	
+	for ( int i = 0; i < Number; i++ ){			//Q14
+	
+	System.out.println(num1 + " ");
+
+	int num3 = num2 + num1;
+	num1 = num2;
+	num2 = num3;
+	}
+	
+
+
+
+
+	Scanner x = new Scanner(System.in);
+	System.out.println("Enter the Number");
+	int Number = x.nextInt();
+
+	int Fact = 1, i;					//Q15
+
+	for (i = 2; i<= Number; i++){
+		Fact *= i;
+	}
+
+	System.out.println("Factorial of "+Number+"is"+Fact);
+
+
+
+
+
+
+
+
+	Scanner x = new Scanner(System.in);
+
+	System.out.println("Enter the Number");
+	int Number = x.nextInt();			//Q17
+	int i;
+	for (i = 1; i <= Number; i++){
+		System.out.println(i);
+	}
+
+
+
+
+
+
+
+	Scanner x = new Scanner(System.in);
+	System.out.println("Enter the Temp. in Celsius");
+	int Temp = x.nextInt();					//Q18
+	int Fahr = (Temp*9/5)+32;
+	System.out.println(Temp+"C is equal to "+Fahr+"F");
+
+
+
+
+
+	Scanner x = new Scanner(System.in);
+	System.out.println("Enter the Base Number");
+	int Base = x.nextInt();
+	System.out.println("Enter the Exponent Number");	//Q19
+	int Exponent = x.nextInt();
+
+	int Pow = 1;
+	for (int i = 1; i <= Exponent; i++){
+		Pow *= Base;
+
+		}
+	
+	System.out.println(Base+" raised to the power "+Exponent+" is "+Pow);
+
+
+
+
+
+
+	Scanner x = new Scanner(System.in);
+	System.out.println("Enter the Number");
+	int Number = x.nextInt();
+	
+	int num1 = Number;
+	int Count = 0;						//Q20
+	while (num1 != 0){
+	num1 = num1/10;
+	Count++;
+	}
+
+	System.out.println("The Number " + Number +" has " + Count + " Digits");
+
+
+
+
+*/
+
+
+
+	}
+
+
+}
