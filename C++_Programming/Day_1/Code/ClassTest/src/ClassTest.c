91ab09309af9dd142a8a0301d5ac2f49f426cc28
amolgavit26 <amolgavit158121@gmail.com>
2025-06-06 23:40:49 +0530

    CPP Day_1 & Day_2

---
+++ C++_Programming/Day_1/Code/ClassTest/src/ClassTest.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : ClassTest.c
+ Author      : Amol Gavit
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
