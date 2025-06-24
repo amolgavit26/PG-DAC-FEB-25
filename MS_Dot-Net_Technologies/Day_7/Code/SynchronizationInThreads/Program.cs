1c00a51a725e9c2b6e2f88666b1434048084b83a
amolgavit26 <amolgavit158121@gmail.com>
2025-06-24 22:26:51 +0530

    DNET Day_7

---
+++ MS_Dot-Net_Technologies/Day_7/Code/SynchronizationInThreads/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SynchronizationInThreads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
