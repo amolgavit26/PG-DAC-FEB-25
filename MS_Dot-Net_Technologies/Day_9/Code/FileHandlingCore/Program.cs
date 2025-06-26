0fc5a77cc226e949722654506cdd1de8f379d3be
amolgavit26 <amolgavit158121@gmail.com>
2025-06-26 23:56:12 +0530

    DNET Day_9

---
+++ MS_Dot-Net_Technologies/Day_9/Code/FileHandlingCore/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace FileHandingInCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteToFile();
+            ReadFromFile();
+        }
+
+        private static void WriteToFile()
+        {
+            StreamWriter writer = File.CreateText("C:\\aaaa\\a.txt");
+            writer.WriteLine("Hello World");
+            writer.WriteLine("Line 2");
+            writer.WriteLine("Done for the day");
+
+            writer.Close();
+        }
+        private static void ReadFromFile()
+        {
+            string s;
+            StreamReader reader = File.OpenText("C:\\aaaa\\a.txt");
+            while ((s = reader.ReadLine()) != null)
+            {
+                Console.WriteLine(s);
+            }
+            reader.Close();
+        }
+    }
+}
+
+
+
+// TODO
+/*
+1)
+Create an employee object and persist it
+Read from the file and recreate the employee object
+
+2)
+Create a list of employees and persist it
+Read from that file and recreate the object.
+ */
