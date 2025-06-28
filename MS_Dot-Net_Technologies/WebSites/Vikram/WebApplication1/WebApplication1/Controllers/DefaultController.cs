1e610b98d33c904dfc8b4f36ed9a534abfdade26
amolgavit26 <amolgavit158121@gmail.com>
2025-06-28 23:28:49 +0530

    DNET Day_12 WebSite

---
+++ MS_Dot-Net_Technologies/WebSites/Vikram/WebApplication1/WebApplication1/Controllers/DefaultController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index(int? id, int a=30, int b=40)
+        {
+            return View();
+
+            
+        }
+    }
+}
